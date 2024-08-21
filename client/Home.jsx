c11d9b2d81c84eb7411505ffa9f46d4428975d91
Surendar022002 <surendarvpk@gmail.com>
2024-08-21 17:04:31 +0530

    commit

---
+++ client/Home.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import "./Login.css";
+import { Link } from "react-router-dom";
+import { Login } from "./Login";
+import { Routes, Route } from "react-router-dom";
+import { SignUp } from "./SignUp";
+import sec21 from "../assests/sec21.png";
+import log from "../assests/logo_food_bg.png"
+import food1 from "../assests/tri.jpg"
+import food2 from "../assests/wings.jpg"
+
+
+export const Home = () => {
+  return (
+    <>
+      <header className="header">
+        <nav className="navbar">
+        <img src={log} alt="logo" className="logo"/>
+          <ul className="nav-item">
+            <li className="nav-item">
+              <a href="www.google.com" className="nav-link">
+                Home
+              </a>
+              <a href="www.google.com" className="nav-link">
+                Product
+              </a>
+              <a href="www.google.com" className="nav-link">
+                Services
+              </a>
+              <a href="www.google.com" className="nav-link">
+                Contact
+              </a>
+            </li>
+          </ul>
+          <Link to={"/login"} className="btn">
+            Login
+          </Link>
+        </nav>
+      </header>
+      <section className="container">
+        <div className="dark">
+          <h1 className="title">
+            SP<span className="span">EC</span>S F
+            <span className="span">OO</span>D
+          </h1>
+          <p className="para">
+            S-P-E-C is a premier catering and events company of choice in
+            Chennai. We create remarkable experiences by offering the finest
+            quality of foods. With detailed planning, unparallel personal
+            services, exquisite dining and precise implementation we set the
+            standard for the memorable occassion. We invite you to create,
+            experience and celebrate the extraordinary.
+          </p>
+        </div>
+
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<SignUp />} />
+        </Routes>
+      </section>
+      <section className="container-sec2">
+        <div className="dark-sec2">
+          <h3 className="sub-head">GET INSPIRED</h3>
+          <h1 className="head">Delicious Food</h1>
+          <img src={sec21} alt="logo" className="image"/>
+          <p className="para-sec2"></p>
+        </div>
+      </section>
+      <section>
+        <div className="list">
+          <div className="box">
+            <img src={food1} alt="logo"/>
+            <h2>---F00D---</h2>
+          </div>
+          <div className="box">
+            <img src={food2} alt="logo"/>
+            <h2>---F00D---</h2>
+          </div>
+          
+
+        </div>
+     </section>
+    </>
+  );
+};
---
+++ client/SignUp.jsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from "react";
+import "./Login.css";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+
+axios.defaults.baseURL = "http://localhost:1100/";
+
+export const SignUp = () => {
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const [signUpData, setSignUpData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const togglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const getTransition = () => {
+    const timer = setTimeout(() => setIsVisible(true), 100);
+    return () => clearTimeout(timer);
+  };
+
+  const handleSignUp = (e) => {
+    const { name, value } = e.target;
+    setSignUpData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = await axios.post("/signup", signUpData);
+    console.log(data);
+    navigate("/login");
+  };
+
+  useEffect(() => {
+    getTransition();
+  }, []);
+
+  return (
+    <div className="dark">
+      <div className={`form-container ${isVisible ? "active" : ""}`}>
+        <Link to={"/home"}>
+          <i className="uil uil-times form-close"></i>
+        </Link>
+        <div className="form signup-form">
+          <form onSubmit={handleSubmit}>
+            <h2>Signup</h2>
+
+            <div className="input-box">
+              <input
+                type="text"
+                name="name"
+                placeholder="Enter Your Name"
+                value={signUpData.name}
+                onChange={handleSignUp}
+                required
+              />
+              <i class="uil uil-user-circle user"></i>
+            </div>
+            <div className="input-box">
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter Your Email"
+                value={signUpData.email}
+                onChange={handleSignUp}
+                required
+              />
+              <i className="uil uil-envelope email"></i>
+            </div>
+            <div className="input-box">
+              <input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                placeholder="Create Password"
+                value={signUpData.password}
+                onChange={handleSignUp}
+                required
+              />
+              <i className="uil uil-lock password"></i>
+              <Link onClick={togglePassword} className="pwd-hide">
+                <i
+                  className={showPassword ? "uil uil-eye" : "uil uil-eye-slash"}
+                ></i>
+              </Link>
+            </div>
+            <button className="btn">Signup Now</button>
+          </form>
+          <div className="login-signup">
+            Already have an account?{" "}
+            <Link to={"/login"} id="login">
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
